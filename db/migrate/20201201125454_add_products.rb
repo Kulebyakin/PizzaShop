bf3f3927c1b0f8ba64af7b2f3a817749a43e37e8
Kulebyakin Andrey <ankulebyakin@gmail.com>
2020-12-01 21:55:28 +0800

    Change path to image in migration

--- db/migrate/20201201125454_add_products.rb
+++ db/migrate/20201201125454_add_products.rb
@@ -1,6 +1,6 @@
 class AddProducts < ActiveRecord::Migration[6.0]
   def change
-  	Product.create :title => 'Hawaiian', :description => 'This is Hawaiian pizza', :price => 350, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/image/hawaiian.jpg'
+  	Product.create :title => 'Hawaiian', :description => 'This is Hawaiian pizza', :price => 350, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/image/hawaiian.png'
 
 	Product.create :title => 'Pepperoni', :description => 'Nice Pepperoni pizza', :price => 450, :size => 30, :is_spicy => true, :is_veg => false, :is_best_offer => true, :path_to_image => '/image/pepperoni.jpg'
 
