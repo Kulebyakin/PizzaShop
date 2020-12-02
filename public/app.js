d4542f255a6c9d34251ebfabb33ac8c901e7e478
Kulebyakin Andrey <ankulebyakin@gmail.com>
2020-12-02 19:58:29 +0800

    Add function to onClick on button "Add to cart"

--- public/app.js
+++ public/app.js
@@ -1,10 +1,16 @@
+function add_to_cart(id)
+{
+	alert(id);
+}
+
 function something() 
-	{
-		var x = window.localStorage.getItem('bbb');
+{
+	var x = window.localStorage.getItem('bbb');
 
-		x = x * 1 + 1;
+	x = x * 1 + 1;
 
-		window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('bbb', x);
 
-		alert(x);
-	}
+	alert(x);
+}
+
