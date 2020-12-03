b392e629c8187cbd3ba54e5396979fb6112faa52
Kulebyakin Andrey <ankulebyakin@gmail.com>
2020-12-03 11:34:32 +0800

    Add cart_number_of_items function

--- public/app.js
+++ public/app.js
@@ -4,4 +4,16 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	alert('Number of items in your cart: ' + cart_number_of_items());
+}
+
+function cart_number_of_items()
+{
+	var sum = 0;
+	for (var i = 0; i < localStorage.length; i++)
+	{
+    	sum = sum * 1 + localStorage.getItem(localStorage.key(i)) * 1;
+	}
+	return sum;
 }
